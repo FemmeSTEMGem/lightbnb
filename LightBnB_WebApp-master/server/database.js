e125bda40cfa7516032c32fef5a0043b1caa9ca1
FemmeSTEMGem <femmestemgem@gmail.com>
2022-02-11 07:51:29 -0500

    Updated getUserWithID and addUser with query code

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -52,10 +52,24 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+
+const getUserWithId = (id) => {
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then((result) => {
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 }
+
 exports.getUserWithId = getUserWithId;
+
+// const getUserWithId = function(id) {
+//   return Promise.resolve(users[id]);
+// }
+// exports.getUserWithId = getUserWithId;
 
 
 /**
@@ -63,13 +77,28 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+
+const addUser = (user) => {
+  return pool
+  .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password])
+  .then ((result) => {
+    console.log(result)
+    return result.rows[0]
+  })
+  .catch((err) => {
+    console.log(err.message);
+  })
 }
 exports.addUser = addUser;
+
+
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+// exports.addUser = addUser;
 
 /// Reservations
 
